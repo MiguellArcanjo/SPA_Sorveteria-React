a9d6f0b0634ec7a74537e780c94fa41175f6760e
Miguel Arcanjo <gmrziko82@gmail.com>
2023-07-08 23:11:35 -0300

    end

--- meu-app/src/componentes/paginas/Sobre/index.js
+++ meu-app/src/componentes/paginas/Sobre/index.js
@@ -1,9 +1,52 @@
 import React from "react";
+import './style.css';
+
+import Topo from '../../Topo';
+import Rodape from '../../Rodape';
 
 export default function Sobre() {
     return (
         <div>
-            SObre
+            <Topo />
+
+            <main>
+                <section className="bannerSobre">
+                    <h1>A GELATERIA</h1>
+                </section>
+
+                <section className="conteudoSobre">
+                    <div className="limita-secao">
+                        <h1>Sobre Nós</h1>
+                        <span>Nós simplesmente amamos sorvete!</span>
+                        <p>Somos uma empresa apaixonada pelo que faz. Colocamos amor em cada
+                        sorvete produzido. Fazemos o melhor sorvete para os nossos
+                        clientes e gostamos de receber elogios. Estamos operando desde
+                        2009 com as melhores matérias-primas para a produção final do
+                        sorvete. Vendemos tanto para varejo como para atacado.</p>
+
+                        <p>Nossos clientes podem comprar os nossos sorvetes e degustar na
+                        nossa loja ou levar para sua residência e aproveitar junto com a
+                        família. Também vendemos para estabelecimentos e criamos eventos
+                        como festa de aniversário, formaturas e eventos empresariais. Para
+                        contratar os nossos serviços, basta entrar em contato conosco.
+                        Iremos proporcionar o melhor atendimento e os melhores produtos
+                        para você fazer a sua festa mais saborosa, com o melhor sorvete da
+                        cidade.</p>
+                    </div>
+                </section>
+
+                <section className="imgsSobre">
+                    <div className="img1">
+                        <img src="assets/sobre-image.jpg" alt=""/>
+                    </div>
+                    
+                    <div>
+                        <img src="assets/sorveteria.jpg" alt="" />
+                    </div>
+                </section>
+            </main>
+
+            <Rodape />
         </div>
     )
 }
